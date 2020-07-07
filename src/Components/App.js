617b4db3f8a3b099fc0dbb2909482bc8ce7b496a
freddy <copamamanifreddy@gmail.com>
2020-07-06 20:27:44 -0400

    Se instala node-sass y se importa el archivo styles.scss en index.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => (
-  <h1>plataforma de educacion online</h1>
+  <h1>Plataforma de educación online</h1>
 )
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-
+import "./styles/styles.scss"
 
 ReactDOM.render(
   <React.StrictMode>
