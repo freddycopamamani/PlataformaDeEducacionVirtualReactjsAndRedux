bf752d0bca2fb2541c0cd941743aa152d0829ac2
freddy <copamamanifreddy@gmail.com>
2020-07-11 12:12:24 -0400

    Crea todos los componentes de las paginas privadas de la aplicacion

---
+++ src/Components/Pages/Course.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Course = () => (
+  <div className="ed-grid">
+    <h1>Pagina de cada curso</h1>
+  </div>
+)
+
+export default Course
---
+++ src/Components/Pages/Fragment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fragment = () => (
+  <div className="ed-grid">
+    <h1>Soy una clase</h1>
+  </div>
+)
+
+export default Fragment
